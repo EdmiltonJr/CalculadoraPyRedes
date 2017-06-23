4d71177d2cbc62bfbf18822111ebf5880a50f88a
EdmiltonJr <jedmiltonjr@gmail.com>
2017-06-22 23:23:08 -0300

    Arquivos da calculadora

---
+++ ServidorUDP.py
@@ -0,0 +1,51 @@
+import socket
+from math import sqrt
+import os
+
+def calcSoma(a, b):
+    c = float(a) + float(b)
+    return str(c)
+
+def calcRaiz(a):
+    return str(sqrt(float(a)))
+
+def operCalc(operacao):
+    lOperacao = operacao.split()
+    if lOperacao[0].upper() == 'SOMA':
+        if lOperacao[1].isnumeric() and lOperacao[2].isnumeric():
+            return calcSoma(lOperacao[1], lOperacao[2])
+        else:
+            sErro = 'Os valores passados não são numéricos.'
+            return sErro
+    elif lOperacao[0].upper() == 'RAIZ':
+        if lOperacao[1].isnumeric():
+            return calcRaiz(lOperacao[1])
+        else:
+            sErro = 'Os valores passados não são numéricos.'
+            return sErro
+    else:
+        sErro = 'Mensagem inválida'
+        return sErro
+
+
+os.system('cls' if os.name == 'nt' else 'clear')
+serverHost = 'localhost'
+serverPort = 3600
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind((serverHost, serverPort))
+
+print("Servidor Iniciado.")
+
+while True:
+    strComandos, clientAddress = serverSocket.recvfrom(2048)
+    print('Conexão iniciada por: ', clientAddress)
+    lComandos = strComandos.decode('utf-8').rstrip('\n').split('\n')
+    result = ''
+    print(lComandos)
+    for comOper in lComandos:
+        result += 'Comando \'' + comOper + '\' Resultado: ' + operCalc(comOper) + '\n'
+    print(result)
+    serverSocket.sendto(result.encode(), clientAddress)
+    serverSocket.close()
+    break
